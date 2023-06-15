472522fda8d62e2d89f7ba2dc6a2b2cb7fa5a5c8
iamfyke <fyketonel@gmail.com>
2023-06-16 00:26:39 +0800

    Employee (Log-in, Room Management,and Table Viewing)

--- src/HotelReservation.java
+++ src/HotelReservation.java
@@ -4,4 +4,14 @@ import java.util.regex.*;
 
 
 public class HotelReservation {
+    private static final Scanner sc = new Scanner(System.in);
+    private static String customerName, phoneNumber;
+
+    public static void main(String[] args) {
+
+    }
+
+    private static void userRegistration() {
+
+    }
 }
