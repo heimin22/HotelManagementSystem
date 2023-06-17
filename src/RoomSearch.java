321be607b29105a79f8f798a3edb28fa3c4937ae
iamfyke <fyketonel@gmail.com>
2023-06-17 19:06:58 +0800

    Employee (Log-in, Room Management,and Table Viewing)

--- src/RoomSearch.java
+++ src/RoomSearch.java
@@ -4,10 +4,11 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class RoomSearch {
     private List<Room> rooms;
-    private Connection connection;
+    private static Connection connection;
 
     public RoomSearch(Connection connection) {
         this.connection = connection;
@@ -58,11 +59,11 @@ public class RoomSearch {
         return availableRooms;
     }
 
-    public boolean reserveRoom(int roomNumber, int days, int nights) {
+    public boolean reserveRoom(int roomNumber, int days, int nights) throws SQLException {
         Room room = getRoomByNumber(roomNumber);
         if (room != null && !room.isOccupied()) {
             room.setOccupied(true);
-            String reservationId = generateReservationID();
+            int reservationId = generateReservationID();
             System.out.println("Reservation successful!" + "\nReservation ID: " + reservationId);
             return true;
         }
@@ -81,7 +82,57 @@ public class RoomSearch {
         return null;
     }
 
-    private String generateReservationID() {
+    private static int generateReservationID() throws SQLException {
+        try {
+        // this is the set of the following numbers in a string type
+        String existingIDs = "0123456789";
+        // maximum id length is 4
+        int idLength = 4;
 
+        // accessing the customer ID
+        String sql = "SELECT MAX (room_id) FROM \"hotelReservationOfficial\".\"hotelSchema\".rooms";
+
+        // establishing the statement connection
+        Statement statement = connection.createStatement();
+
+        // // the resultset will be used to execute the following SQL statement to access the following variables
+        ResultSet resultSet = statement.executeQuery(sql);
+
+        // 0 is the default value
+        int maxRoomID = 0;
+        // if there's an available slot for the customer id then the statement below will be executed
+        if (resultSet.next()) {
+            maxRoomID = resultSet.getInt(1);
+        }
+
+        // increment it to 1 to grant the system to generate a unique ID
+        int newUserID = maxRoomID + 1;
+
+        statement.close();
+        resultSet.close();
+
+        // creating a StringBuilder object
+        StringBuilder sb = new StringBuilder();
+        // random object
+        Random random = new Random();
+
+        // generating a unique ID until it reaches the limit
+        for (int i = 0; i < idLength; i++) {
+            int index = random.nextInt(existingIDs.length());
+            sb.append(existingIDs.charAt(index));
+        }
+
+        // the following unique ID that is an integer before will now be converted into a String
+        String uniqueIDString = sb.toString();
+        int uniqueID = Integer.parseInt(uniqueIDString);
+
+        return uniqueID;
+    }
+        catch (SQLException e) {
+        e.printStackTrace();
+    }
+
+    // return 0 if an error occurs
+        return 0;
     }
 }
